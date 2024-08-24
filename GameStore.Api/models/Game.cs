360d0416546f5415b5b1929035670f54892bc358
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2024-08-24 23:52:31 +0200

    feat: add crud functionality

--- GameStore.Api/models/Game.cs
+++ GameStore.Api/models/Game.cs
@@ -4,9 +4,9 @@ public class Game
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public string Genre { get; set; }
+    public required string Genre { get; set; }
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
-    public string required ImageUri { get; set; }
+    public required string ImageUri { get; set; }
 
 }
