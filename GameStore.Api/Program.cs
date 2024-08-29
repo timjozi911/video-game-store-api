22afeebd9915fefa3c21db7f0063392be5cf85eb
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2024-08-29 16:53:34 +0200

    feat: add db connection

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,7 @@ using GameStore.Api.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepo, InMemoryRepo>();
+var connString = builder.Configuration.GetConnectionString("GameStoreContext");
 
 var app = builder.Build();
 app.MapGamesEndpoints();
